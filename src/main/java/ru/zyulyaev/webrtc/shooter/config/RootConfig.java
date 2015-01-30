1483e15cd9be2326c951e0cc18c75a65e3c85081
Nikita Zyulyaev <nikzyul@ya.ru>
2015-01-30 21:02:04 +0300

    caching removed

--- src/main/java/ru/zyulyaev/webrtc/shooter/config/RootConfig.java
+++ src/main/java/ru/zyulyaev/webrtc/shooter/config/RootConfig.java
@@ -1,8 +1,5 @@
 package ru.zyulyaev.webrtc.shooter.config;
 
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -15,15 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @Import(WebConfig.class)
 @EnableScheduling
-@EnableCaching
 public class RootConfig {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager();
     }
 }
